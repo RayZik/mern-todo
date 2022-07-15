44b2caef5235a874475530603491390ff1bc0d4b
RayZik <a.zikrackiy@gmail.com>
2022-07-15 08:18:22 +0800

    [BE]: Update stress endpoint

--- api/index.ts
+++ api/index.ts
@@ -6,7 +6,6 @@ import { apiRouter } from './routes';
 import { AwsSecretManager } from './lib/aws/secret-manager';
 
 
-
 AwsSecretManager.attachToProcessEnv('MernProd').then(() => {
 	const port = 3001;
 	const app = express();
@@ -14,24 +13,27 @@ AwsSecretManager.attachToProcessEnv('MernProd').then(() => {
 	app.use(express.json());
 	app.use(cors());
 
-	mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@mern-db-cluster.cluster-cmsh7c30416b.us-east-1.docdb.amazonaws.com:27017/mern-todo?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false`, {
-		tlsCAFile: 'rds-combined-ca-bundle.pem'
-	}).then(() => {
+	mongoose
+		.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@mern-db-cluster.cluster-cmsh7c30416b.us-east-1.docdb.amazonaws.com:27017/mern-todo?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false`, {
+			tlsCAFile: 'rds-combined-ca-bundle.pem'
+		}).then(() => {
+			console.log("Connected to MongoDB", new Date());
 
-		console.log("Connected to MongoDB", new Date());
+			app.use('/api', apiRouter);
 
-		app.use('/api', apiRouter);
+			app.get('/stress', (req, res) => {
+				const startedAt = Date.now()
+				while (Date.now() - startedAt <= 10000) { /* do nothing*/ }
 
-		app.get('/stress', (req, res) => {
-			res.status(200).send('OK');
-		});
+				res.status(200).send('OK');
+			});
 
-		app.get('/health-check', (req, res) => {
-			res.status(200).send('OK');
-		});
+			app.get('/health-check', (req, res) => {
+				res.status(200).send('OK');
+			});
 
-		app.listen(port, () => {
-			console.log(`Started on ${port}`);
-		});
-	}).catch(console.error);
+			app.listen(port, () => {
+				console.log(`Started on ${port}`);
+			});
+		}).catch(console.error);
 });
